ddf2c38efd359dd08c17e2e305e7f2f84da90dee
beimnet worku <130739098+beimnet101@users.noreply.github.com>
2025-01-20 15:56:19 +0300

    edit footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,17 +10,17 @@ const Footer = () => {
                 <div className="text-center text-xs leading-5 space-y-2">
                     <p>In Loving Memory of Tigist Deselagn ❤️</p>
                     
-                    <p>1. **Privacy and Data Collection**: We collect information to improve your shopping experience, including your name, email, and order details. For more details, please refer to our Privacy Policy.</p>
+                    <p>1. Privacy and Data Collection: We collect information to improve your shopping experience, including your name, email, and order details. For more details, please refer to our Privacy Policy.</p>
 
-                    <p>2. **Returns and Refunds**: You may return most items within 30 days of delivery for a full refund. Items must be returned in new, unused condition. Some restrictions may apply.</p>
+                    <p>2. Returns and Refunds: You may return most items within 30 days of delivery for a full refund. Items must be returned in new, unused condition. Some restrictions may apply.</p>
 
-                    <p>3. **Shipping Information**: We offer various shipping options, including standard and expedited shipping, depending on your location and the availability of items.</p>
+                    <p>3. Shipping Information: We offer various shipping options, including standard and expedited shipping, depending on your location and the availability of items.</p>
 
-                    <p>4. **Product Availability**: All products are subject to availability and may be limited in quantity. We reserve the right to cancel or modify orders as necessary.</p>
+                    <p>4. Product Availability: All products are subject to availability and may be limited in quantity. We reserve the right to cancel or modify orders as necessary.</p>
 
-                    <p>5. **Emashop Card**: Emashop Card is available for qualifying customers in the United States. Add your Emashop Card to Wallet on supported devices for easy payments.</p>
+                    <p>5. Emashop Card: Emashop Card is available for qualifying customers in the Ethiopia and United States. Add your Emashop Card to Wallet on supported devices for easy payments.</p>
 
-                    <p>6. **Terms and Conditions**: By using our services, you agree to our Terms of Use. Features, applications, and services may vary by region. Policies are subject to change without prior notice.</p>
+                    <p>6. Terms and Conditions: By using our services, you agree to our Terms of Use. Features, applications, and services may vary by region. Policies are subject to change without prior notice.</p>
                 </div>
             </div>
         </footer>
