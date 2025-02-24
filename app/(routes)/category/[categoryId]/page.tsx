ef1b2651c5b439c277b23e0e548fdf05c2e646f2
beimnet worku <130739098+beimnet101@users.noreply.github.com>
2025-02-24 14:09:41 +0300

    mobile update on catagories

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -43,7 +43,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         </div>
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
-          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8 pt-3">
             {/* Mobile filters */}
             <MobileFilter sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,48 @@
+// app/components/navbar.tsx (Server Component)
 import Link from "next/link";
 import Container from "./ui/container";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarAction from "./navbar-action";
 import { UserButton } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import MobileMenu from "./ui/MobileMenu";
 
-export const revalidate=0;
+export const revalidate = 0;
+
 const Navbar = async () => {
-
   const categories = await getCategories();
 
   if (!Array.isArray(categories)) {
     throw new Error("Expected categories to be an array.");
   }
+
   return (
     <div className="border-b">
       <Container>
-        <div className="relative  sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+        <div className="relative sm:px-6 lg:px-8 flex h-16 items-center">
+          {/* Mobile Hamburger Menu */}
+          <MobileMenu categories={categories} />
+
+          {/* Logo */}
+          <Link
+            href="/"
+            className="ml-4 flex lg:ml-0 gap-x-2 absolute left-1/2 transform -translate-x-1/2 lg:static lg:translate-x-0"
+          >
             <p className="font-bold text-xl">EMAMA-STORE</p>
           </Link>
-          <MainNav data={categories} />
-          <NavbarAction/>
-          <div className="text-lg mt-2 ml-2 pr-0">
-          <UserButton afterSignOutUrl="/" />
-          </div> 
-          
+
+          {/* Main Navigation (Desktop Only) */}
+          <div className="hidden lg:flex">
+            <MainNav data={categories} />
+          </div>
+
+          {/* Navbar Actions */}
+          <div className="ml-auto flex items-center">
+            <NavbarAction />
+            <div className="text-lg mt-2 ml-2 pr-0">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          </div>
         </div>
       </Container>
     </div>
